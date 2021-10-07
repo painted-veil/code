39a16a355104d9fd0b1a38661bada3d6150cba12
painted-veil <1037970589@qq.com>
2021-10-07 13:45:41 +0800

    try

---
+++ 数组/209-长度最小数组1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {//暴力解法
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+    }
+};
+int main(int argc, char* argv[]) {
+
+}
